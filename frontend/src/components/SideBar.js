1d27713054b8701f2c776b19d2ecec1f09ba7630
jeussantiago <jeus1santiago@gmail.com>
2023-08-23 03:10:43 -0400

    fixed bug with bootstrap.min.css that didnt have the map file when downloaded. Resolved it by downloading the complete file instead (boostrap.css). I updated the file structures to includes the styles and seperate them by their corresponding component or screen. The Favicon was update. And an image folder was created to store the brand logo which will be used in the home screen

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
+import "../styles/components/SideBar.css";
+
 function SideBar() {
+    const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
+    const [menu_class, setMenuClass] = useState("menu hidden");
+    const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+    // toggle burger menu change
+    const updateMenu = () => {
+        if (!isMenuClicked) {
+            setBurgerClass("burger-bar clicked");
+            setMenuClass("menu visible");
+        } else {
+            setBurgerClass("burger-bar unclicked");
+            setMenuClass("menu hidden");
+        }
+        setIsMenuClicked(!isMenuClicked);
+    };
+
+    // Show sidebar when screen size is not tablet size
+    // returns a function to cleanup/remove event listener
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 576) {
+                setBurgerClass("burger-bar clicked");
+                setMenuClass("menu visible");
+                setIsMenuClicked(true);
+            }
+        };
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     const logoutHandler = () => {
         console.log("loggin out");
         // dispatch(logout());
@@ -11,92 +46,102 @@ function SideBar() {
 
     return (
         <div>
-            <div className="container-fluid">
-                <div className="row ">
-                    <div className="col-auto col-sm2 bg-light d-flex flex-column justify-content-between min-vh-100">
-                        {/* <div className="mt-2 px-4"> */}
-                        <div className="upper-sidebar-content">
-                            <div className="text-decoration-none ms-3 d-flex align-itmes-center d-none d-sm-inline">
-                                <LinkContainer to="/dashboard">
-                                    <h3 className="brand-name">
-                                        Coin Companion
-                                    </h3>
-                                </LinkContainer>
+            <div className="burger-container d-block d-sm-none">
+                <div className="burger-box" onClick={updateMenu}>
+                    <div className="burger-menu">
+                        <div className={burger_class}></div>
+                        <div className={burger_class}></div>
+                        <div className={burger_class}></div>
+                    </div>
+                </div>
+            </div>
+            <div className={menu_class}>
+                <div className="container-fluid">
+                    <div className="row ">
+                        <div className="col-auto col-sm2 bg-light d-flex flex-column justify-content-between min-vh-100">
+                            <div className="upper-sidebar-content">
+                                <div className="text-decoration-none ms-3 d-flex align-itmes-center d-none d-sm-inline">
+                                    <LinkContainer to="/dashboard">
+                                        <h3 className="brand-name">
+                                            Coin Companion
+                                        </h3>
+                                    </LinkContainer>
+                                </div>
+                                <ul
+                                    className="nav nav-pills flex-column mt-2"
+                                    id="parentM"
+                                >
+                                    <li className="nav-item">
+                                        <LinkContainer
+                                            to="/dashboard"
+                                            className="nav-link"
+                                        >
+                                            <div>
+                                                <i className="fa-solid fa-tachograph-digital"></i>
+                                                <span className="ms-2 d-none d-sm-inline">
+                                                    Dashboard
+                                                </span>
+                                            </div>
+                                        </LinkContainer>
+                                    </li>
+                                    <li className="nav-item">
+                                        <LinkContainer
+                                            to="/friends"
+                                            className="nav-link"
+                                        >
+                                            <div>
+                                                <i className="fas fa-user-friends"></i>
+                                                <span className="ms-2 d-none d-sm-inline">
+                                                    Friends
+                                                </span>
+                                            </div>
+                                        </LinkContainer>
+                                    </li>
+                                    <li className="nav-item">
+                                        <LinkContainer
+                                            to="/groups"
+                                            className="nav-link"
+                                        >
+                                            <div>
+                                                <i className="fa-solid fa-users-line"></i>
+                                                {/* <i class="fa-solid fa-people-group"></i> */}
+                                                <span className="ms-2 d-none d-sm-inline">
+                                                    Groups
+                                                </span>
+                                            </div>
+                                        </LinkContainer>
+                                    </li>
+                                    <li className="nav-item">
+                                        <LinkContainer
+                                            to="/settings"
+                                            className="nav-link"
+                                        >
+                                            <div>
+                                                <i className="fa-solid fa-gear"></i>
+                                                <span className="ms-2 d-none d-sm-inline">
+                                                    Settings
+                                                </span>
+                                            </div>
+                                        </LinkContainer>
+                                    </li>
+                                </ul>
                             </div>
-                            <ul
-                                className="nav nav-pills flex-column mt-2"
-                                id="parentM"
-                            >
-                                <li className="nav-item">
-                                    <LinkContainer
-                                        to="/dashboard"
-                                        className="nav-link"
-                                    >
-                                        <div>
-                                            <i className="fa-solid fa-tachograph-digital"></i>
-                                            <span className="ms-2 d-none d-sm-inline">
-                                                Dashboard
+                            <div className="lower-sidebar-content">
+                                <div className="text-decoration-none">
+                                    <div className="d-grid gap-2">
+                                        <button
+                                            className="btn btn-outline-primary"
+                                            type="button"
+                                            onClick={logoutHandler}
+                                        >
+                                            <span className="d-none d-sm-inline">
+                                                Logout
                                             </span>
-                                        </div>
-                                    </LinkContainer>
-                                </li>
-                                <li className="nav-item">
-                                    <LinkContainer
-                                        to="/friends"
-                                        className="nav-link"
-                                    >
-                                        <div>
-                                            <i className="fas fa-user-friends"></i>
-                                            <span className="ms-2 d-none d-sm-inline">
-                                                Friends
+                                            <span className="d-block d-sm-none">
+                                                <i className="fa-solid fa-right-from-bracket"></i>
                                             </span>
-                                        </div>
-                                    </LinkContainer>
-                                </li>
-                                <li className="nav-item">
-                                    <LinkContainer
-                                        to="/groups"
-                                        className="nav-link"
-                                    >
-                                        <div>
-                                            <i className="fa-solid fa-users-line"></i>
-                                            {/* <i class="fa-solid fa-people-group"></i> */}
-                                            <span className="ms-2 d-none d-sm-inline">
-                                                Groups
-                                            </span>
-                                        </div>
-                                    </LinkContainer>
-                                </li>
-                                <li className="nav-item">
-                                    <LinkContainer
-                                        to="/settings"
-                                        className="nav-link"
-                                    >
-                                        <div>
-                                            <i className="fa-solid fa-gear"></i>
-                                            <span className="ms-2 d-none d-sm-inline">
-                                                Settings
-                                            </span>
-                                        </div>
-                                    </LinkContainer>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="lower-sidebar-content">
-                            <div className="text-decoration-none">
-                                <div className="d-grid gap-2">
-                                    <button
-                                        className="btn btn-outline-primary"
-                                        type="button"
-                                        onClick={logoutHandler}
-                                    >
-                                        <span className="d-none d-sm-inline">
-                                            Logout
-                                        </span>
-                                        <span className="d-block d-sm-none">
-                                            <i className="fa-solid fa-right-from-bracket"></i>
-                                        </span>
-                                    </button>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import "./bootstrap.min.css";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
+import "./styles/bootstrap.css";
+import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
